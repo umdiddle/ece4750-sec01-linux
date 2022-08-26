962ea77087c87e878ddd4ae6e0420d6ef7532669
gtz4 <gtz4@cornell.edu>
2022-08-26 15:28:23 -0400

    c code added

---
+++ avg-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int avg( int x, int y)
+{
+    int sum = x + y;
+    return sum/2;
+}
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = avg( a,b );
+    printf( "average of %d and %d is %d\n", a, b, c );
+    return 0;
+}
+
+// unsigned int avg( unsigned int x, unsigned int y )
+// {
+//   unsigned int sum = x + y;
+//   return sum / 2;
+// }
